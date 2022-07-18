62a6bca9a044989c3f3d40d0c19bdb27004feb9f
Benjamin Hitimana <benjamin@Benjamins-MacBook-Air.local>
2022-07-18 09:00:58 +0200

    phone number validator

--- models/admin.js
+++ models/admin.js
@@ -1,10 +1,35 @@
 const {Schema, model} = require('mongoose')
-const adminSchema = new Schema({ firstname: { type: String, required: [true, ''] }, 
- lastname: { type: String, required: [true, ''] },
- email: { type: String, required: [true, ''] }, 
- phone: { type: String, required: [true, '']
+const adminSchema = new Schema({ 
+    firstname: { 
+        type: String, 
+        required: [true, ''] 
+    }, 
+    lastname: { 
+        type: String, 
+        required: [true, ''] 
+    },
+    email: { 
+        type: String, 
+        required: [true, ''] 
+    },
+    phone: { 
+        type: String, 
+        required: [true, ''],
+        validate: {
+            validator : function (phone) {
+                validPhone == /^[0-9]{10}$/;
+                if (!phone == validPhone){
+                    console.log('Incorrect phone number')
+                }
+                else {
+                    console.log('Phone number entered correctly!')
+                }
+            }
+        },
  }, 
- address: { type: String, required: [true, '']
+ address: { 
+    type: String, 
+    required: [true, '']
  }
 })
 const Admins = model('admin', adminSchema)
